ba1ba7c84d732066b2de34174f527e82ea00db97
Stanislas Ormières <stan@stormier.ninja>
2023-05-07 11:36:18 +0200

    chore: :wrench: Add vitepress config

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Conventions projets JS/TS",
+  description: "Conventions du Ministère de l’intérieur",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'client', link: '/client' },
+      { text: 'serveur', link: '/serveur' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Recettes client',
+        items: [
+          { text: 'Vue 3', link: '/client/vue3-recettes' },
+          { text: 'Nuxt 3', link: '/client/nuxt3-recettes' }
+        ]
+      },
+      {
+        text: 'Recettes serveurs',
+        items: [
+          { text: 'NestJS', link: '/serveurs/nestjs-recettes' },
+          { text: 'Fastify', link: '/serveurs/fastify-recettes' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
